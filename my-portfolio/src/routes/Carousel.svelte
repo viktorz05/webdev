751c0aa7e6dddba2c2c45dad44a4d67d528f684d
Viktor Zepeda <vza12@sfu.ca>
2025-11-20 23:04:08 -0800

    got the cards and possibly scroll button

---
+++ my-portfolio/src/routes/Carousel.svelte
@@ -0,0 +1,45 @@
+<div class = "carousel">
+    <div class ="card"> lolollololololoo</div>
+    <div class ="card"> penis</div>
+    <div class ="card"> penis</div> 
+</div>
+
+<style>
+    .carousel {
+    width: 100%;
+    overflow-x: auto;
+	margin: 3em auto;
+	display: flex;
+	gap: 1em;
+    scroll-behavior: smooth;
+}
+
+.carousel::-webkit-scrollbar {
+    display: none;
+}
+
+.carousel::scroll-button(right) {
+    content: "\2192";
+    border: none;
+    background-color: rgb(255, 0, 0);
+    font-size: 3em;
+    height: 60px;
+    width: 60px;
+    border-radius: 50%;
+    padding-bottom: .1em;
+    cursor: pointer;
+    position: fixed;
+    display: block;
+}
+.card {
+	flex: 0 0 20em;
+	height: 12em;
+	background-color: dimgray;
+	padding: 1em;
+	border-radius: .5em;
+	text-align: center;
+	align-content: center;
+    color:black;
+}
+</style>
+
